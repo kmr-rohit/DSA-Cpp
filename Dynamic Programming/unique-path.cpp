c33957f24582b3e28556e7fa4959eee08565a959
rk972006 <rk972006@student.nitw.ac.in>
2022-04-10 17:27:43 +0530

    Update unique-path.cpp

--- Dynamic Programming/unique-path.cpp
+++ Dynamic Programming/unique-path.cpp
@@ -22,6 +22,28 @@ public:
     }
 };
 
+// Recursive bottom up approach 
+class Solution {
+public:
+    int solve(int i , int j ,int m , int n){
+        if(i == 0 && j == 0){
+            return 1;
+
+        }
+        else if(i<0 || j<0){
+            return 0;
+        }
+        
+        int left = solve(i, j-1, m, n);
+        int up = solve(i-1 , j , m ,n);
+        return up +left;
+        
+    }
+    int uniquePaths(int m, int n) {
+        return solve(0,0,m,n);
+    }
+};
+
 // Memoisation Approach 
 
 class Solution {
